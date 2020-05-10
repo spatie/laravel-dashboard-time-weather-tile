b31671a7c4363bc10b94f060ee8a146990d3e9af
Alex <alex@whatcheer.com>
2020-05-10 16:19:43 -0500

    WIP

--- src/Services/OpenWeatherMap.php
+++ src/Services/OpenWeatherMap.php
@@ -8,7 +8,7 @@ class OpenWeatherMap
 {
     public static function getWeatherReport(string $key, string $city, string $units): array
     {
-        $url = "https://api.openweathermap.org/data/2.5/weather?q={$city}&appid={$key}&units={$key}";
+        $url = "https://api.openweathermap.org/data/2.5/weather?q={$city}&appid={$key}&units={$units}";
 
         return Http::get($url)->json();
     }
