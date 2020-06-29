bd125d8280eb17aa4c2ee272c79903e2cfd1bdb5
Roel Pennings <roelpennings@hotmail.com>
2020-06-29 13:46:15 +0200

    Update Buienradar.php

--- src/Services/Buienradar.php
+++ src/Services/Buienradar.php
@@ -6,20 +6,24 @@ use Illuminate\Support\Facades\Http;
 
 class Buienradar
 {
-    public static function getForecasts(string $latitude, string $longitude): array
+    public static function getForecasts(string $latitude, string $longitude)
     {
         $endpoint = "https://graphdata.buienradar.nl/forecast/json/?lat={$latitude}&lon={$longitude}";
-
+        
         $response = Http::get($endpoint)->json();
-
-        return collect($response['forecasts'])
-            ->map(function (array $forecast) {
-                return [
-                    'time' => $forecast['datetime'],
-                    'rain' => $forecast['precipitation'],
-                ];
-            })
-            ->take(12)
-            ->toArray();
+        
+        if (Http::get($endpoint)->ok() && count($response) > 0) {
+            return collect($response['forecasts'])
+                ->map(function (array $forecast) {
+                    return [
+                        'time' => $forecast['datetime'],
+                        'rain' => $forecast['precipitation'],
+                    ];
+                })
+                ->take(12)
+                ->toArray();
+        }
+        
+        return false;
     }
 }
