0fbcf5685040e3cd0b26f133674635545982b799
Roel Pennings <roelpennings@hotmail.com>
2020-06-29 13:55:27 +0200

    Update Buienradar.php

--- src/Services/Buienradar.php
+++ src/Services/Buienradar.php
@@ -10,18 +10,20 @@ class Buienradar
     {
         $endpoint = "https://graphdata.buienradar.nl/forecast/json/?lat={$latitude}&lon={$longitude}";
         
-        $response = Http::get($endpoint)->json();
-        
-        if (Http::get($endpoint)->ok() && count($response) > 0) {
-            return collect($response['forecasts'])
-                ->map(function (array $forecast) {
-                    return [
-                        'time' => $forecast['datetime'],
-                        'rain' => $forecast['precipitation'],
-                    ];
-                })
-                ->take(12)
-                ->toArray();
+        if (Http::get($endpoint)->ok()) {
+            $response = Http::get($endpoint)->json();
+            
+            if (count($response) > 0) {
+                return collect($response['forecasts'])
+                    ->map(function (array $forecast) {
+                        return [
+                            'time' => $forecast['datetime'],
+                            'rain' => $forecast['precipitation'],
+                        ];
+                    })
+                    ->take(12)
+                    ->toArray();
+            }
         }
         
         return false;
